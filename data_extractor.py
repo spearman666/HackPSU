cccc0c7c27c3cf1c9d7de0b44fb65ab9f8763798
Marshall Malino <mr.marshalls.email@gmail.com>
2018-10-07 12:58:09 -0400

    Update data_extractor.py

--- data_extractor.py
+++ data_extractor.py
@@ -1,53 +1,61 @@
+from decimal import *
 import re
+from operator import itemgetter
 file = open('academic_report.txt',mode='r')
 yeet = file.read()
 file.close()
 
+#getting total credits needed
+#total = re.search(r"(?:[a-zA-Z'-]+[^a-zA-Z'-]+){0,10}units are required for the degree(?:[a-zA-Z'-]+[^a-zA-Z'-]+){0,10}",yeet)
 GAs = re.findall(r'Not Satisfied Complete(.*?)GA',yeet)
 if not GAs:
     GAs = 'Satisfied'
 else:
-    GAs = GAs[0]
-                 
+    GAs = GAs[0].strip()
+    
 GNs = re.findall(r'Not Satisfied Complete(.*?)GN',yeet)
 if not GNs:
     GNs = 'Satisfied'
 else:
-    GNs = GNs[0]
-                 
+    GNs = GNs[0].strip()
 GHs = re.findall(r'Not Satisfied Complete(.*?)GH',yeet)
 if not GHs:
     GHs = 'Satisfied'
 else:
-    GHs = GHs[0]
-                 
+    GHs = GHs[0].strip()
+    
 GSs = re.findall(r'Not Satisfied Complete(.*?)GS',yeet)
 if not GSs:
-   GSs = 'Satisfied'
+    GSs = 'Satisfied'
 else:
-    GSs = GSs[0]
-
+    GSs = GSs[0].strip()
+    
 GHAs = re.findall(r'Not Satisfied Complete(.*?)GHA',yeet)
 if not GHAs:
     GHAs = 'Satisfied'
 else:
-    GHAs = GHAs[0]    
+    GHAs = GHAs[0].strip()
     
 USs = re.findall(r'Not Satisfied Complete(.*?)US',yeet)
 if not USs:
     USs = 'Satisfied'
 else:
-    USs = USs[0]
-
+    USs = USs[0].strip()
+    
 ILs = re.findall(r'Not Satisfied Complete(.*?)IL',yeet)
 if not ILs:
     ILs = 'Satisfied'
 else:
-    ILs = ILs[0]
-                 
+    ILs = ILs[0].strip()
+i=0
+tri = ()
 liss = (GNs,GAs,GHs,GSs,GHAs,USs)
+liss
 print(liss)
+firebase.put('/Gen Eds',"Gen Eds", {'GN':GNs,'GA':GAs,'GH':GHs,'GS':GSs,'GHA':GHAs,'US':USs,'IL':ILs})
 
-#getting the total number of credits needed for the major, how many you've taken, and how many you still need
+
+
 total = re.findall(' Units:(.*?)required,(.*?)used,(.*?)needed',yeet)
-max(total,key=itemgetter(1))
+finished = max(total,key=itemgetter(1))
+firebase.put('/Credits','credits',{'Total Needed':finished[0],'Completed':finished[1],'Needed':finished[2]})
